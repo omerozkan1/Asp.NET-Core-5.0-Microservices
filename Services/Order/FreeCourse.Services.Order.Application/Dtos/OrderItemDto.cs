0d61ceba1c34d2d512a7639de0e4f3e43185824d
omer <gs_omer_ozkan@hotmail.com>
2021-04-18 03:15:50 +0300

    Order service and entities were created with the domain driven design structure.

---
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FreeCourse.Services.Order.Application.Dtos
+{
+    public class OrderItemDto
+    {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public Decimal Price { get; private set; }
+    }
+}
