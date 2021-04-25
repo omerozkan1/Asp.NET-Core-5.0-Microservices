e045ac3077c60e312f1b1759447b2603463e8958
omer <gs_omer_ozkan@hotmail.com>
2021-04-25 16:23:27 +0300

    Written identity service interface i methods

---
+++ Clients/FreeCourse.Web/Models/ServiceApiSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Web.Models
+{
+    public class ServiceApiSetting
+    {
+        public string BaseUri { get; set; }
+        public string PhotoStockUri { get; set; }
+    }
+}
---
+++ Clients/FreeCourse.Web/Models/SigninInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
