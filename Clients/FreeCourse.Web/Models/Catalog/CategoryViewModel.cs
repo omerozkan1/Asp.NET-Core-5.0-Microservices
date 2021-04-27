aa74cb3505fb00c06559b255ba87f813496d1df0
omer <gs_omer_ozkan@hotmail.com>
2021-04-28 00:59:15 +0300

    ICatalogService interface created and method signatures created

---
+++ Clients/FreeCourse.Web/Models/Catalog/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Web.Models.Catalog
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Clients/FreeCourse.Web/Models/Catalog/CourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FreeCourse.Web.Models.Catalog;
+using System;
+
+namespace FreeCourse.Web.Models.Catalog
+{
+    public class CourseViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+        public string CategoryId { get; set; }
+        public CategoryViewModel Category { get; set; }
+        public FeatureViewModel Feature { get; set; }
+    }
+}
