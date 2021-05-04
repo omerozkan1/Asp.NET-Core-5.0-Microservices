e3fb70320b00d64da855281c32269d727900d47e
omer <gs_omer_ozkan@hotmail.com>
2021-05-04 23:21:30 +0300

    basket controller apply&cancel discount

--- Clients/FreeCourse.Web/Controllers/BasketController.cs
+++ Clients/FreeCourse.Web/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.Web.Models.Basket;
+using FreeCourse.Web.Models.Discount;
 using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -43,6 +44,20 @@ namespace FreeCourse.Web.Controllers
             if (!result)
                 return NotFound();
 
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> ApplyDiscount(DiscountApplyInput discountApplyInput)
+        {
+            var discountStatus = await _basketService.ApplyDiscount(discountApplyInput.Code);
+            TempData["DiscountStatus"] = discountStatus;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> CancelApplyDiscount()
+        {
+            await _basketService.CancelApplyDiscount();
             return RedirectToAction(nameof(Index));
         }
     }
